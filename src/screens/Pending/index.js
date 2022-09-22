f75a8aa68690e618e4bb509c8fe7efb17ab8c264
James <james@Jamess-MBP-2.lan>
2022-09-22 17:46:39 +0100

    .

--- src/screens/Pending/index.js
+++ src/screens/Pending/index.js
@@ -1,40 +1,64 @@
-import React from 'react';
-import { CardHeader, Box, Card, Divider, Typography, CardContent, CircularProgress } from '@mui/material';
-import { useSearchParams } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
-import { setup } from '../../stores/user';
+import React from "react";
+import {
+  CardHeader,
+  Box,
+  Card,
+  Divider,
+  Typography,
+  CardContent,
+  CircularProgress,
+} from "@mui/material";
+import { useSearchParams } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { setup } from "../../stores/user";
 
 const Pending = () => {
   const [searchParams, setSearchParams] = useSearchParams();
 
   const dispatch = useDispatch();
 
-  const email = searchParams.get('email');
-  const firstName = searchParams.get('firstName');
-  const token = searchParams.get('token');
+  const email = searchParams.get("email");
+  const firstName = searchParams.get("firstName");
+  const token = searchParams.get("token");
 
   if (token) {
-    window.localStorage.setItem('token', token);
+    window.localStorage.setItem("token", token);
     dispatch(setup());
-    window.open('http://localhost:3000', '_self');
+    window.open("https://sprightly-pithivier-b6b45f.netlify.app", "_self");
   }
 
   return (
-    <Box sx={{ display: 'flex', height: '90vh', alignItems: 'center', justifyContent: 'center' }}>
+    <Box
+      sx={{
+        display: "flex",
+        height: "90vh",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
       <Card sx={{ width: 500, height: 500 }}>
         <CardHeader title="Verify your email" />
         <Divider />
 
         <CardContent>
-          <Typography sx={{ fontWeight: 400, fontSize: '20px' }}>{`👋 Hey there,`}</Typography>
+          <Typography
+            sx={{ fontWeight: 400, fontSize: "20px" }}
+          >{`👋 Hey there,`}</Typography>
           <Typography
             sx={{
               fontWeight: 400,
-              fontSize: '15px',
-              color: '#00000090',
-              marginTop: '20px',
-            }}>{`We've sent an email to ${email}. Please click the email link to complete your sign in.`}</Typography>
-          <Box sx={{ display: 'flex', justifyContent: 'center', marginTop: '100px' }}>
+              fontSize: "15px",
+              color: "#00000090",
+              marginTop: "20px",
+            }}
+          >{`We've sent an email to ${email}. Please click the email link to complete your sign in.`}</Typography>
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+              marginTop: "100px",
+            }}
+          >
             <CircularProgress size={50} />
           </Box>
         </CardContent>
--- src/stores/user/index.js
+++ src/stores/user/index.js
@@ -1,21 +1,23 @@
-import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
-import call from '../../utils/call';
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import call from "../../utils/call";
 
 // Gets token from localstorage, verifies and returns session
-export const setup = createAsyncThunk('user/setup', async () => {
-  const token = localStorage.getItem('token');
-  const { session, workers, actions } = await call('POST', 'user/setup', { token });
+export const setup = createAsyncThunk("user/setup", async () => {
+  const token = localStorage.getItem("token");
+  const { session, workers, actions } = await call("POST", "user/setup", {
+    token,
+  });
   return { session, workers, actions };
 });
 
 // Sends magic links
-export const auth = createAsyncThunk('user/auth', async (data) => {
-  const response = await call('POST', 'user/auth', data);
+export const auth = createAsyncThunk("user/auth", async (data) => {
+  const response = await call("POST", "user/auth", data);
   return response;
 });
 
 export const user = createSlice({
-  name: 'user',
+  name: "user",
   initialState: {
     session: null,
     errored: false,
@@ -33,11 +35,13 @@ export const user = createSlice({
     builder.addCase(auth.fulfilled, (state, action) => {
       const { email, firstName } = action.payload;
 
-      const redirect = new URL('http://localhost:3000/pending');
-      redirect.searchParams.append('email', email);
-      redirect.searchParams.append('firstName', firstName);
+      const redirect = new URL(
+        "https://sprightly-pithivier-b6b45f.netlify.app/pending"
+      );
+      redirect.searchParams.append("email", email);
+      redirect.searchParams.append("firstName", firstName);
 
-      window.open(redirect, '_self');
+      window.open(redirect, "_self");
     });
   },
 });
